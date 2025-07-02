a4f5785557ab0708ad64fb8124a20e236a8523a1
Mazen Fady <mazenfady85@gmail.com>
2025-07-02 22:00:45 +0300

    Q1)  is solved

--- C# 04/Program.cs
+++ C# 04/Program.cs
@@ -4,14 +4,24 @@ namespace Demo
     internal class program
     {
         #region Q1)
-        public static void main()
+        private  static  void Main()
         {
-
+            //WeekDays W = new WeekDays();
+            string[] W =Enum.GetNames(typeof(WeekDays));
+            foreach(string WeekDays in W) 
+            Console.WriteLine(WeekDays);
 
         }
         public enum WeekDays 
         {
-        
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7,
+
         }
 
         #endregion
