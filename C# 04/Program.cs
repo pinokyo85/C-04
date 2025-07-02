8dd07148d8e47cf431f874e9bc40d786e9411d1d
Mazen Fady <mazenfady85@gmail.com>
2025-07-02 22:26:35 +0300

    Q2) is solved

--- C# 04/Program.cs
+++ C# 04/Program.cs
@@ -4,26 +4,57 @@ namespace Demo
     internal class program
     {
         #region Q1)
-        private  static  void Main()
-        {
-            //WeekDays W = new WeekDays();
-            string[] W =Enum.GetNames(typeof(WeekDays));
-            foreach(string WeekDays in W) 
-            Console.WriteLine(WeekDays);
+        //private  static  void Main()
+        //{
+        //    //WeekDays W = new WeekDays();
+        //    string[] W =Enum.GetNames(typeof(WeekDays));
+        //    foreach(string WeekDays in W) 
+        //    Console.WriteLine(WeekDays);
 
-        }
-        public enum WeekDays 
-        {
-            Monday = 1,
-            Tuesday = 2,
-            Wednesday = 3,
-            Thursday = 4,
-            Friday = 5,
-            Saturday = 6,
-            Sunday = 7,
+        //}
+        //public enum WeekDays 
+        //{
+        //    Monday = 1,
+        //    Tuesday = 2,
+        //    Wednesday = 3,
+        //    Thursday = 4,
+        //    Friday = 5,
+        //    Saturday = 6,
+        //    Sunday = 7,
 
-        }
+        //}
 
         #endregion
 
-}   }
+        #region Q2)
+
+        private static void Main()
+        {
+            Person[] P = new Person[3];
+            P[0] = new Person("Mazen",20);
+            P[1] = new Person("Ashraf", 20);
+            P[2] = new Person("Omar", 20);
+
+            foreach (Person p in P)
+            {
+                Console.WriteLine($"Name : {p.Name}  \nAge : {p.Age}");
+            }
+        }
+        public struct Person
+        {
+            public string Name;
+            public int Age;
+
+            public Person(string name, int age) 
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+
+
+
+        #endregion
+
+    }
+}
